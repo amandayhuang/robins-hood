84cb4efc84120e6d4d789fa5df5998c3fd39683e
amanda <amandayhuang@gmail.com>
2020-04-08 10:29:24 -0400

    backend for stock show

--- app/controllers/api/stocks_controller.rb
+++ app/controllers/api/stocks_controller.rb
@@ -1,4 +1,17 @@
-class StocksController < ApplicationController
+class Api::StocksController < ApplicationController
+    
+    def index
 
+    end
 
+    def show
+        @stock = Stock.find_by(id:params[:id])
+        debugger
+        if @stock
+            # render :template => "/api/stocks/show"
+            render :show
+        else
+            render json: ['Stock not found.'], status: 404
+        end
+    end
 end
---
+++ app/views/api/stocks/show.json.jbuilder
@@ -0,0 +1,2 @@
+# json.partial! 'api/stocks/stock', stock: @stock
+json.extract! @stock, :id, :display_name, :ticker_name
