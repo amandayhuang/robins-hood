145c18586bf93f2b9312281f6eba1d207a1f5dd3
amanda <amandayhuang@gmail.com>
2020-04-08 18:50:47 -0400

    chart styling

--- app/frontend/components/chart.jsx
+++ app/frontend/components/chart.jsx
@@ -5,27 +5,47 @@ import {
 
 const data = [
     {
-        name: 'March 1',  price: 2400
+        name: 'March 1',  $: 2400
     },
     {
-        name: 'March 2',  price: 1398
+        name: 'March 2', $: 1398
     },
     {
-        name: 'March 3',  price: 9800
+        name: 'March 3', $: 9800
     },
     {
-        name: 'March 4',  price: 3908
+        name: 'March 4', $: 3908
     },
     {
-        name: 'March 5',  price: 4800
+        name: 'March 5', $: 4800
     },
     {
-        name: 'March 6',  price: 3800
+        name: 'March 6', $: 3800
     },
     {
-        name: 'March 7',  price: 4300 
+        name: 'March 7', $: 4300 
     },
 ];
+
+const itemStyle = {
+    color: 'gray',
+    'font-family':  'Helvetica Neue',
+    'font-size':'12px'
+};
+
+const wrapperStyle = {
+    color: 'green'
+};
+
+const contentStyle = {
+    border: 'none'
+};
+
+const labelStyle = {
+    display: 'none'
+};
+
+
 
 export default class Chart extends PureComponent {
 
@@ -39,8 +59,8 @@ export default class Chart extends PureComponent {
                     top: 5, right: 30, left: 20, bottom: 20,
                 }}
             >
-                <Tooltip />
-                <Line type="monotone" dot={false} dataKey="price" stroke="#52CF9A" activeDot={{ r: 5 }} />
+                <Tooltip offset='40' separator='' itemStyle={itemStyle} wrapperStyle={wrapperStyle} contentStyle={contentStyle} labelStyle={labelStyle}/>
+                <Line type="monotone"  dot={false} dataKey="$" stroke="#52CF9A" activeDot={{ r: 5 }} />
             </LineChart>
         );
     }
