253af8e85b38489139aa3a869662902d12b31308
amanda <amandayhuang@gmail.com>
2020-06-02 16:50:43 -0700

    remove debuggers

--- app/controllers/api/externals_controller.rb
+++ app/controllers/api/externals_controller.rb
@@ -3,23 +3,24 @@ class Api::ExternalsController < ApplicationController
     def index
         displayName = params[:displayName]
         startDate = params[:startDate]
-        news_api = News.new('060db197a76147cea83f519ea566585c');
         api_key = 'ec885fa30bfd47ea9ca9a19c922c974e';
-        # debugger
+    
         if displayName && startDate
+            # get data for share price
             url = "http://newsapi.org/v2/everything?q=#{displayName}&from=#{startDate}&to=#{startDate}&sortBy=publishedAt&apiKey=#{api_key}"
             response = RestClient.get url
             json = JSON.parse response
-            # debugger
+
             render json: json
         elsif displayName 
+            # get articles for a displayName
             url = "http://newsapi.org/v2/everything?q=#{displayName}&sortBy=popularity&apiKey=#{api_key}"
             response = RestClient.get url
             json = JSON.parse response
 
             render json: json
         else
-            # news = news_api.get_top_headlines(category:'entertainment', country:'us')
+            # get entertainment articles
              url = "http://newsapi.org/v2/top-headlines?country=us&category=entertainment&sortBy=publishedAt&apiKey=#{api_key}"
              response = RestClient.get url
              json = JSON.parse response
